076ee1ccfdf1f7e891ac3c8c09e598ae6003e7ff
Amogh Mahadevan <amogh.mahadevan1@gmail.com>
2023-08-08 18:46:35 -0700

    Initial Commit

---
+++ StadiumScout/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StadiumScout
+//
+//  Created by Amogh Mahadevan  on 8/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StadiumScout/StadiumScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  StadiumScoutApp.swift
+//  StadiumScout
+//
+//  Created by Amogh Mahadevan  on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct StadiumScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
